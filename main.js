19495328e75d2a4a720a0810f9fb66c27422a0dc
RickyAnchores <ricky23anchores@gmail.com>
2021-01-30 20:25:03 +0000

    added js custom lightMode

--- main.js
+++ main.js
@@ -9,3 +9,25 @@ const navSlide = () =>{
 }
 
 navSlide();
+
+
+// APPLY Change THEME 
+/*
+const lightMode = () => {
+    const navbar = document.querySelector('#navbar');
+    const footer = document.querySelector('#footer');
+    const logo = document.querySelector('.logo');
+
+    logo.addEventListener('click', () => {
+        document.body.style.background = "#333";
+        document.body.style.color = "#ccc";
+        navbar.style.background = "white";
+        navbar.style.color = "black";
+        footer.style.background = "white";
+        footer.style.color = "black"
+    });
+    
+}
+
+lightMode();
+*/
