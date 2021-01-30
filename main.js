4cac31ac592af14abcc6d3c69a3df13e06a90350
RickyAnchores <ricky23anchores@gmail.com>
2021-01-30 03:20:21 +0000

    nav slider( ) + nav bg and footer bg set black

---
+++ main.js
@@ -0,0 +1,11 @@
+//NAV SLIDE FUNCTION
+const navSlide = () =>{
+    const burger = document.querySelector('.hamburger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    })
+}
+
+navSlide();
