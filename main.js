d796f91ca8bf752fb07b48b80c674282f0eded3c
RickyAnchores <ricky23anchores@gmail.com>
2021-02-01 01:15:46 +0000

    added themes

--- main.js
+++ main.js
@@ -31,3 +31,58 @@ const lightMode = () => {
 
 lightMode();
 */
+
+// themeButton 1 (turn TEAL)
+
+const tealTheme1 = () => {
+    //button custom
+    const themeBtn = document.querySelector('.themeButton');
+    // Nav and Footer and Logo
+    const navbar = document.querySelector('#navbar');
+    const footer = document.querySelector('#footer');
+    const logo = document.querySelector('.logo');
+    const logoFooter = document.querySelector('.logo-footer');
+
+    // Add Event 
+    themeBtn.addEventListener('click', () => {
+
+        navbar.style.background = "whitesmoke";
+        navbar.style.color = "teal";
+        footer.style.background = "whitesmoke";
+        footer.style.color = "teal";
+        logo.style.background = "whitesmoke";
+        logo.style.color = "teal";
+        logo.style.borderColor = "teal";
+        logoFooter.style.color = "teal";
+        logoFooter.style.borderColor = "teal";
+    })
+}
+
+tealTheme1();
+// themeButton 2 (turn Normal)
+
+const tealTheme2 = () => {
+    //button custom
+    const themeBtn = document.querySelector('.themeButton2');
+    // Nav and Footer and Logo
+    const navbar = document.querySelector('#navbar');
+    const footer = document.querySelector('#footer');
+    const logo = document.querySelector('.logo');
+    const logoFooter = document.querySelector('.logo-footer');
+
+    // Add Event 
+    themeBtn.addEventListener('click', () => {
+
+        navbar.style.background = "";
+        navbar.style.color = "";
+        footer.style.background = "";
+        footer.style.color = "";
+        logo.style.background = "";
+        logo.style.color = "";
+        logo.style.borderColor = "";
+        logoFooter.style.color = "";
+        logoFooter.style.borderColor = "";
+    })
+}
+
+tealTheme2();
